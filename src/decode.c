01c807ff86c6ceab7e4d388203c84d46332b8c7a
Jean Gravier <2744572+jevarg@users.noreply.github.com>
2022-06-26 23:17:57 +0200

    Update decode.c

--- src/decode.c
+++ src/decode.c
@@ -3,14 +3,14 @@
 #include <string.h>
 #include <limits.h>
 
-#include "decode.h"
+#include "audio_decoder.h"
 
 #define PTR_LOBYTE (UINTPTR_MAX >> ((CHAR_BIT * sizeof(void *)) / 2))
 #define reverse_bytes_16(num) (((num & 0xFF00) >> 8) | ((num & 0x00FF) << 8))
 #define HIBYTE(x) (x >> 8)
 #define LOBYTE(x) (x << 8)
 #define HI_NIBBLE(b) (((b) >> 4) & 0x0F)
-#define LO_NIBBLE(b) ((b)&0x0F)
+#define LO_NIBBLE(b) ((b) & 0x0F)
 
 static int8_t gLookupTable[] = {
         0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff,
@@ -48,106 +48,171 @@ static int8_t gLookupTable[] = {
 };
 
 void decode(int8_t *buffer, int8_t *destBuffer) {
-    int16_t ax = *((uint16_t *) buffer + 3);
+//    int16_t ax = *((uint16_t *) buffer + 3);
     uint16_t outSize = *(int16_t *) buffer; // 27FF == 10239
     uint16_t v56 = (uint8_t) buffer[4];
 
-    int8_t *v4 = (int8_t *) &ax;
-    int8_t *hgh = (int8_t *) &ax + 1;
-    *v4 = buffer[8];
+//    int8_t v4 = (int8_t *) &ax;
+//    int8_t *hgh = (int8_t *) &ax + 1;
+    int8_t v4 = buffer[8];
     int8_t *data = &buffer[9];
-    *destBuffer = *v4;
+    *destBuffer = v4;
     int8_t *dest = &destBuffer[1];
 
     uint16_t v7 = (uint8_t) buffer[8];
     uint16_t remainingSize = outSize - 1;
-    uint16_t v12 = 0;
     int16_t counter = 0;
 
     int8_t *ebx = NULL;
 
     if (remainingSize) {
-        *hgh = *v4;
+//        *hgh = v4;
         do {
             while (1) {
                 while (1) {
-                    *v4 = *data++;
-                    if (!(*v4 & 0x80)) {
+                    v4 = *data++;
+                    if (!(v4 & 0x80)) { // Checking if negative bit is set (0x80 == 0b10000000)
                         break;
                     }
 
-                    v7 = (uint8_t) (2 * *v4);
-                    *dest++ = 2 * *v4;
+                    // We left shift byte by 1 (* 2 is same as << 1)
+                    // Note that we cast to 1 byte size.
+                    // Seems like we try to get rid of 1st byte
+                    // in order to keep result it 1 byte long
+                    v7 = (uint8_t) (v4 * 2);
+                    *dest++ = (uint8_t) (v4 * 2);
                     if (!--remainingSize) {
                         return;
                     }
                 }
 
-                if (!(*v4 & 0x40)) {
+                // Checking if second bit is set (0x40 == 0b01000000)
+                if (!(v4 & 0x40)) {
                     break;
                 }
 
-                // int16_t v10 = remainingSize;
-                int length = *v4 & 0x3F;
-                memset(dest, v7, length);
-                dest += length;
+                // We keep the last 6 bits. (0x3F == 0b00111111)
+                // And store it as length
+                int length = v4 & 0x3F;
 
-                // remainingSize = v10 - length;
+                // We write {length} times the current value
+                memset(dest, v7, length);
+                dest += length; // Our dest pointer is moved
+
+                // Have we decoded all bytes?
                 if (remainingSize == length) {
                     return;
                 }
 
+                // We subtract the number of bytes decoded.
                 remainingSize -= length;
             }
 
-            // ebx = &gLookupTable[(*v4 & 0xF) * 16];
-            int test = (*v4 & 0xF) * 16;
+            // ebx = &gLookupTable[(v4 & 0xF) * 16];
+            /**
+             * We keep the low nibble (lower-half of byte)
+             * of the current value and shift it to the left by 4.
+             * e.g:
+             *     0x3d (0b00111101)
+             *   & 0x0f (0b00001111)
+             *   = 0x0d (0b00001101)
+             * <<4 0xd0 (0b11010000)
+             */
+            int test = (v4 & 0xF) << 4;
             ebx = &gLookupTable[test];
-            *v4 &= 0x30u; // is highest byte 16, 32 or 48?
-            counter = remainingSize;
-            if (*v4 == 16) {
-                remainingSize = v56 >> 3; // v56 comes from file data directly.
-                v12 = gLookupTable[test + 8];
-                do {
-                    *v4 = *data++;
-                    ax = reverse_bytes_16(ax);
-//          int16_t qwe = *v4;
-//          *v4 = (uint8_t)v7;
-                    for (size_t i = 1; i <= 8; i++) {
 
-                        if ((*hgh <<= 1) < 0) {
+            /**
+             * We check the 2 last bits of the high nibble
+             * of the current value.
+             * see if conditions a bit later...
+             */
+            v4 &= 0x30u; // is highest byte 16, 32 or 48?
+            counter = remainingSize;
+
+            /**
+             * Is the current flag == 16
+             * means that 2 last bits
+             * of high nibble were == 01
+             * ex: 0x18 (0b00[01]1000)
+             *
+             * For each byte, we check from
+             * left to right if which bits are set.
+             * 
+             * If a bit is set, we add the lookup
+             * table data to the result.
+             * If not, we subtract it from the result.
+             */
+            if (v4 == 0x10) {
+                // printf("0x10\n");
+                remainingSize = v56 >> 3; // v56 comes from file data directly.
+                uint16_t v12 = (uint16_t)gLookupTable[8];
+                do {
+                    v4 = *data++;
+                    for (size_t i = 0; i < 8; i++) {
+                        /**
+                         * For each iteration, we shift left using i
+                         * before checking if the MSB is set.
+                         * This is how we test if each bit is set.
+                         * Example with v4 == 0x18 (00011000)
+                         *  i == 0: 00011000 (24)
+                         *  i == 1: 00110000 (48)
+                         *  i == 2: 01100000 (96)
+                         *  i == 3: 11000000 (-64)
+                         *  i == 4: 10000000 (-128)
+                         *  i == 5: 00000000 (0)
+                         *  i == 6: 00000000 (0)
+                         */
+                        if ((v4 << i) < 0) {
                             v7 += v12;
                             if (HIBYTE(v7)) {
                                 v7 = 0xFF;
                             }
-                        } else {
+                        }
+                        else {
                             v7 -= v12;
                             if (v7 < 0) {
                                 v7 = 0;
                             }
                         }
 
-                        *v4 = v7;
-                        *dest++ = (int8_t) v7;
-                        printf("ASD\n");
+                        *dest++ = (int8_t)v7;
                     }
                     --remainingSize;
                 } while (remainingSize);
-            } else if (*v4 == 32) {
-                int8_t *ptr = ((uintptr_t) ebx & ~PTR_LOBYTE) | (uintptr_t) (ebx + 6);
+            }
+                /**
+                 * Is the current flag == 32
+                 * means that 2 last bits
+                 * of high nibble were == 10
+                 * ex: 0x28 (0b00[10]1000)
+                 *
+                 * For each byte, we get each half-nibble (2 bits)
+                 * in order to calculate its result
+                 */
+            else if (v4 == 32) {
+                // printf("32\n");
+                int8_t *ptr = &ebx[6];
+                // int8_t *ptr = ((uintptr_t) ebx & ~PTR_LOBYTE) | (uintptr_t) (ebx + 6);
                 // ebx = ((uintptr_t)ebx & ~PTR_LOBYTE) | (uintptr_t)(ebx + 6);
                 // int8_t *ptr = ebx;
-                remainingSize = v56 >> 2; // TODO: Check what v56 is. it comes from the file directly
+                remainingSize = v56 >> 2;
 
                 do {
                     uint8_t b = *data++;
-                    *v4 = ptr[b >> 6];
-                    if (*v4 & 0x80) {
-                        *hgh = 0xff;
-                    } else {
-                        *hgh = 0;
-                    }
-                    v7 += *v4;
+
+                    /**
+                     * we look for an association of
+                     * the MSB of current
+                     * value's high nibble in lookup table.
+                     * Right shifting by 6 allows to
+                     * get those bits.
+                     *
+                     * e.g with b = 0x90:
+                     *      0x90 (0b[10]010000)
+                     *  >>6 0x02 (0b000000[10])
+                     */
+                    v4 = ptr[b >> 6];
+                    v7 += v4;
                     if (v7 < 0) {
                         v7 = 0;
                     }
@@ -155,15 +220,24 @@ void decode(int8_t *buffer, int8_t *destBuffer) {
                         v7 = 0xFF;
                     }
 
+                    /**
+                     * We add the byte-clamped result
+                     * in the result buffer
+                     */
                     *dest++ = (int8_t) v7;
 
-                    *v4 = ptr[b >> 4 & 3];
-                    if (*v4 & 0x80) {
-                        *hgh = 0xff;
-                    } else {
-                        *hgh = 0;
-                    }
-                    v7 += *v4;
+                    /**
+                     * we look for an association of
+                     * the LSB of current
+                     * value's high nibble in lookup table.
+                     *
+                     * e.g with b = 0x90:
+                     *      0x90 (0b10[01]0000)
+                     *  >>4 0x09 (0b0000[1001])
+                     *  & 3 0x01 (0b000000[01])
+                     */
+                    v4 = ptr[b >> 4 & 3];
+                    v7 += v4;
                     if (v7 < 0) {
                         v7 = 0;
                     }
@@ -171,15 +245,24 @@ void decode(int8_t *buffer, int8_t *destBuffer) {
                         v7 = 0xFF;
                     }
 
+                    /**
+                     * We add the byte-clamped result
+                     * in the result buffer
+                     */
                     *dest++ = (int8_t) v7;
 
-                    *v4 = ptr[b >> 2 & 3];
-                    if (*v4 & 0x80) {
-                        *hgh = 0xff;
-                    } else {
-                        *hgh = 0;
-                    }
-                    v7 += *v4;
+                    /**
+                    * we look for an association of
+                    * the MSB of current
+                    * value's low nibble in lookup table.
+                    *
+                    * e.g with b = 0x90:
+                    *      0x84 (0b1000[01]00)
+                    *  >>2 0x21 (0b001000[01])
+                    *  & 3 0x01 (0b000000[01])
+                    */
+                    v4 = ptr[b >> 2 & 3];
+                    v7 += v4;
                     if (v7 < 0) {
                         v7 = 0;
                     }
@@ -187,15 +270,23 @@ void decode(int8_t *buffer, int8_t *destBuffer) {
                         v7 = 0xFF;
                     }
 
+                    /**
+                     * We add the byte-clamped result
+                     * in the result buffer
+                     */
                     *dest++ = (int8_t) v7;
 
-                    *v4 = ptr[b & 3];
-                    if (*v4 & 0x80) {
-                        *hgh = 0xff;
-                    } else {
-                        *hgh = 0;
-                    }
-                    v7 += *v4;
+                    /**
+                    * we look for an association of
+                    * the LSB of current
+                    * value's low nibble in lookup table.
+                    *
+                    * e.g with b = 0x42:
+                    *      0x42 (0b010000[10])
+                    *  & 3 0x02 (0b000000[10])
+                    */
+                    v4 = ptr[b & 3];
+                    v7 += v4;
                     if (v7 < 0) {
                         v7 = 0;
                     }
@@ -203,21 +294,42 @@ void decode(int8_t *buffer, int8_t *destBuffer) {
                         v7 = 0xFF;
                     }
 
+                    /**
+                     * We add the byte-clamped result
+                     * in the result buffer
+                     */
                     *dest++ = (int8_t) v7;
                     --remainingSize;
                 } while (remainingSize);
-            } else {
+            }
+                /**
+                * Is the current flag not 16 or 32
+                * means that 2 LSB
+                * of high nibble were == 00
+                * ex: 0x08 (0b00[00]1000)
+                *
+                * For each byte, we get each nibble (4 bits)
+                * in order to calculate its result
+                */
+            else {
+                // printf("other\n");
+                // v56 is divided by 2 (>> 1)
                 remainingSize = (v56 >> 1);
                 do {
                     uint8_t b = *data++;
 
-                    *v4 = ebx[b >> 4];
-                    if (*v4 & 0x80) {
-                        *hgh = 0xff;
-                    } else {
-                        *hgh = 0;
-                    }
-                    v7 += *v4;
+                    /**
+                     * we look for an association of
+                     * the high nibble of current
+                     * value in lookup table.
+                     */
+                    v4 = ebx[b >> 4];
+//                    if (v4 & 0x80) {
+//                        *hgh = 0xff;
+//                    } else {
+//                        *hgh = 0;
+//                    }
+                    v7 += v4;
                     if (v7 < 0) {
                         v7 = 0;
                     }
@@ -225,15 +337,24 @@ void decode(int8_t *buffer, int8_t *destBuffer) {
                         v7 = 0xFF;
                     }
 
+                    /**
+                     * We add the byte-clamped result
+                     * in the result buffer
+                     */
                     *dest++ = (int8_t) v7;
 
-                    *v4 = ebx[b & 0xf];
-                    if (*v4 & 0x80) {
-                        *hgh = 0xff;
-                    } else {
-                        *hgh = 0;
-                    }
-                    v7 += *v4;
+                    /**
+                     * we look for an association of
+                     * the low nibble of current
+                     * value in lookup table.
+                     */
+                    v4 = ebx[b & 0xf];
+//                    if (v4 & 0x80) {
+//                        *hgh = 0xff;
+//                    } else {
+//                        *hgh = 0;
+//                    }
+                    v7 += v4;
                     if (v7 < 0) {
                         v7 = 0;
                     }
@@ -241,8 +362,11 @@ void decode(int8_t *buffer, int8_t *destBuffer) {
                         v7 = 0xFF;
                     }
 
+                    /**
+                     * We add the byte-clamped result
+                     * in the result buffer
+                     */
                     *dest++ = (int8_t) v7;
-                    *hgh = 0;
                     --remainingSize;
                 } while (remainingSize);
             }
@@ -253,12 +377,13 @@ void decode(int8_t *buffer, int8_t *destBuffer) {
 }
 
 int main(int argc, char const *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s FILE\n", argv[0]);
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s IN_FILE OUT_FILE\n", argv[0]);
         return 1;
     }
 
     const char *fileName = argv[1];
+    const char *outFile = argv[2];
     FILE *file = fopen(fileName, "rb");
     if (file == NULL) {
         fprintf(stderr, "Invalid file: %s\n", fileName);
@@ -279,19 +404,19 @@ int main(int argc, char const *argv[]) {
     for (size_t i = 0; i < header.numberOfChunks; i++) {
         fread(&header.chunks[i], sizeof(FileChunk), 1, file);
         // printf("\nChunk #%d/%d\n", i + 1, header.numberOfChunks);
-        printf(
-                "\nChunk #%d/%d\n"
-                "\tOffset: %d\n"
-                "\tEncoded size: %d\n"
-                "\tDecoded size: %d\n",
-                i + 1, header.numberOfChunks,
-                header.chunks[i].offset,
-                header.chunks[i].encodedSize,
-                header.chunks[i].decodedSize
-        );
+        // printf(
+        //         "\nChunk #%d/%d\n"
+        //         "\tOffset: %d\n"
+        //         "\tEncoded size: %d\n"
+        //         "\tDecoded size: %d\n",
+        //         i + 1, header.numberOfChunks,
+        //         header.chunks[i].offset,
+        //         header.chunks[i].encodedSize,
+        //         header.chunks[i].decodedSize
+        // );
     }
 
-    FILE *f = fopen("./out.SND", "w+");
+    FILE *f = fopen(outFile, "w+");
     if (f == NULL) {
         perror("Error file");
         return 1;
